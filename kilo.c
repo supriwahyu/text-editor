992870e124e32271febb7217613fe520608e04fa
supriwahyu <supriwahyu95@gmail.com>
2026-04-20 19:54:07 +0700

    prevent moving cursor offside off screen.

--- kilo.c
+++ kilo.c
@@ -109,20 +109,28 @@ int editorReadKey() {
 
 /*** input ***/
 void editorMoveCursor(int key) {
-  switch (key) {
-    case ARROW_LEFT:
-      E.cx--;
-      break;
-    case ARROW_RIGHT:
-      E.cx++;
-      break;
-    case ARROW_UP:
-      E.cy--;
-      break;
-    case ARROW_DOWN:
-      E.cy++;
-      break;
-  }
+    switch (key) {
+        case ARROW_LEFT:
+            if (E.cx != 0) {
+                E.cx--;
+            }            
+            break;
+        case ARROW_RIGHT:
+            if (E.cx != E.screencols - 1) {
+                E.cx++;
+            }
+            break;
+        case ARROW_UP:
+            if (E.cy != 0) {
+                E.cy--;
+            }
+            break;
+        case ARROW_DOWN:
+            if (E.cy != E.screenrows -1) {
+                E.cy++;
+            }
+            break;
+    }
 }
 
 void editorProcessKeypress() {

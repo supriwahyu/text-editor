89fe1ab47ef4eee451f766a77ec60e24b01f136a
supriwahyu <supriwahyu95@gmail.com>
2026-04-21 09:31:22 +0700

    progress: chapter III

--- kilo.c
+++ kilo.c
@@ -25,7 +25,12 @@ enum editorKey {
   ARROW_LEFT = 1000,
   ARROW_RIGHT,
   ARROW_UP,
-  ARROW_DOWN
+  ARROW_DOWN,
+  DEL_KEY,
+  HOME_KEY,
+  END_KEY,
+  PAGE_UP,
+  PAGE_DOWN
 };
 
 void abAppend(struct abuf *ab, const char *s, int len) {
@@ -71,7 +76,7 @@ void enableRawMode() {
    
    raw.c_iflag &= (BRKINT | ICRNL | INPCK | ISTRIP | IXON);
    raw.c_oflag &= (OPOST);
-   raw.c_cflag != (CS8);
+   raw.c_cflag |= (CS8);
    raw.c_lflag &= (ECHO | ICANON | IEXTEN | ISIG);
 
    raw.c_cc[VMIN] = 0;
@@ -95,16 +100,39 @@ int editorReadKey() {
     if (read(STDOUT_FILENO, &seq[1], 1) != 1) return '\x1b';
 
     if (seq[0] == '[') {
-        switch (seq[1]) {
-            case 'A': return ARROW_UP;
-            case 'B': return ARROW_DOWN;
-            case 'C': return ARROW_RIGHT;
-            case 'D': return ARROW_LEFT;
+        if (seq[1] >= '0' && seq[1] <= '9') {
+            if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+            if (seq[2] == '~') {
+                switch (seq[1]) {
+		    case '1': return HOME_KEY;
+		    case '3': return DEL_KEY;
+		    case '4': return END_KEY;
+                    case '5': return PAGE_UP;
+                    case '6': return PAGE_DOWN;
+		    case '7': return HOME_KEY;
+		    case '8': return END_KEY;
+                }
+            }
+        } else {
+            switch (seq[1]) {
+                case 'A': return ARROW_UP;
+                case 'B': return ARROW_DOWN;
+                case 'C': return ARROW_RIGHT;
+                case 'D': return ARROW_LEFT;
+		case 'H': return HOME_KEY;
+		case 'F': return END_KEY;
+            }
         }
+    } else if (seq[0] == '0') {
+	switch (seq[1]) {
+	    case 'H': return HOME_KEY;
+	    case 'F': return END_KEY;
+	}
     }
   } else {
     return c;
   }
+  return 0;
 }
 
 /*** input ***/
@@ -142,6 +170,22 @@ void editorProcessKeypress() {
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
+
+	case HOME_KEY:
+	    E.cx = 0;
+	    break;
+	case END_KEY:
+	    E.cx = E.screencols - 1;
+	    break;
+
+	case PAGE_UP:
+	case PAGE_DOWN:	
+	    {
+	    	int times = E.screenrows;
+		while (times--)
+		    editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+	    }
+	    break;
 
         case ARROW_UP:
         case ARROW_DOWN:
@@ -225,8 +269,8 @@ int main() {
     initEditor();
 
     while (1) {
-	   editorRefreshScreen();
-       editorProcessKeypress();
+	    editorRefreshScreen();
+	    editorProcessKeypress();
     }
 
     return 0;

cf419ab2cf937b89d86dd3a36e3c66a2f4fda4c5
supriwahyu <supriwahyu95@gmail.com>
2026-04-23 14:21:01 +0700

    add color digit

--- kilo.c
+++ kilo.c
@@ -40,6 +40,7 @@ typedef struct erow {
     int rsize;
     char *chars;
     char *render;
+    unsigned char *hl;
 } erow;
 
 enum editorKey {
@@ -53,6 +54,11 @@ enum editorKey {
     END_KEY,
     PAGE_UP,
     PAGE_DOWN
+};
+
+enum editorHeighlight {
+    HL_NORMAL = 0,
+    HL_NUMBER
 };
 
 void abAppend(struct abuf *ab, const char *s, int len) {
@@ -337,7 +343,30 @@ void editorDrawRows(struct abuf *ab) {
             int len = E.row[filerow].rsize - E.coloff;
             if (len < 0) len = 0;
             if (len > E.screencols) len = E.screencols;
-            abAppend(ab, &E.row[filerow].render[E.coloff], len);
+
+            char *c = &E.row[filerow].render[E.coloff];
+            unsigned char *hl = &E.row[filerow].hl[E.coloff];
+            int current_color = -1;
+            int j;
+            for (j = 0; j < len; j++) {
+                if (hl[j] == HL_NORMAL) {
+                    if (current_color != -1) {
+                        abAppend(ab, "x1b[39m", 5);
+                        current_color != -1;
+                    }
+                    abAppend(ab, &c[j], 1);
+                } else {
+                    int color = editorSyntaxToColor(hl[j]);
+                    if (color != current_color) {
+                        current_color = color;
+                        char buf[16];
+                        int clen = snprintf(buf, sizeof(buf), "\x1b[%dm", color);
+                        abAppend(ab, buf, clen);
+                    }
+                    abAppend(ab, &c[j], 1);
+                }
+            }
+            abAppend(ab, "\x1b[39m", 5);
         }
 
         abAppend(ab, "\x1b[K", 3);
@@ -450,6 +479,8 @@ void editorUpdateRow(erow *row) {
   }
   row->render[idx] = '\0';
   row->rsize = idx;
+
+  editorUpdateSyntax(row);
 }
 
 void editorAppendRow(char *s, size_t len) {
@@ -472,6 +503,7 @@ void editorAppendRow(char *s, size_t len) {
 void editorFreeRow(erow *row) {
   free(row->render);
   free(row->chars);
+  free(row->hl);
 }
 void editorDelRow(int at) {
   if (at < 0 || at >= E.numrows) return;
@@ -500,6 +532,7 @@ void editorInsertRow(int at, char *s, size_t len) {
   E.row[at].chars[len] = '\0';
   E.row[at].rsize = 0;
   E.row[at].render = NULL;
+  E.row[at].hl = NULL;
   editorUpdateRow(&E.row[at]);
   E.numrows++;
   E.dirty++;
@@ -576,13 +609,13 @@ char *editorPrompt(char *prompt, void (*callback)(char *, int)) {
       if (buflen != 0) buf[--buflen] = '\0';
     } else if (c == '\x1b') {
       editorSetStatusMessage("");
-      if (callback) callback(buf, c)
+      if (callback) callback(buf, c);
       free(buf);
       return NULL;
     } else if (c == '\r') {
       if (buflen != 0) {
         editorSetStatusMessage("");
-        if (callback) callback(buf, c)
+        if (callback) callback(buf, c);
         return buf;
       }
     } else if (!iscntrl(c) && c < 128) {
@@ -593,7 +626,7 @@ char *editorPrompt(char *prompt, void (*callback)(char *, int)) {
       buf[buflen++] = c;
       buf[buflen] = '\0';
     }
-    if (callback) callback(buf, c)
+    if (callback) callback(buf, c);
   }
 }
 
@@ -725,6 +758,26 @@ void editorFind() {
     }
 }
 
+/*** syntax heighlight ***/
+void editorUpdateSyntax(erow *row) {
+    row->hl = realloc(row->hl, row->rsize);
+    memset(row->hl, HL_NORMAL, row->rsize);
+
+    int i;
+    for (i = 0; i < row->rsize; i++) {
+        if (isdigit(row->render[i])) {
+            row->hl[i] = HL_NUMBER;
+        }
+    }
+}
+
+int editorSyntaxToColor(int hl) {
+    switch (hl) {
+        case HL_NUMBER: return 31;
+        default: return 37;
+    }
+}
+
 /*** init ***/
 void initEditor() {
     E.cx = 0;
@@ -744,7 +797,6 @@ void initEditor() {
     E.screenrows -= 2;
 }
 
-/*** init ***/
 int main(int argc, char *argv[]) {
     enableRawMode();
     initEditor();
